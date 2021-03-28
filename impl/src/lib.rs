6a2e3d48616d4e6d53c78372aba1007a5c5388c7
blck-snwmn <whitesnowmancreator@gmail.com>
2021-03-29 02:20:34 +0900

    Create target field by macro

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -6,6 +6,34 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn derive_parse(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let input_indent = format_ident!("{}", input.ident);
+
+    let data = match input.data {
+        syn::Data::Struct(s) => Some(s),
+        _ => None,
+    };
+    let data = data.unwrap();
+
+    let mut init_fields = Vec::new();
+    let mut build_fields = Vec::new();
+    for f in data.fields {
+        let filed_indent = f.ident;
+        let filed_ty = f.ty;
+        // 一旦固定値は0で。
+        init_fields.push(quote! {
+            let mut #filed_indent: #filed_ty = 0;
+        });
+        build_fields.push(quote! {
+            #filed_indent
+        });
+    }
+    let init_fields = quote! {
+        #(#init_fields)*
+    };
+
+    let build_fields = quote! {
+        #(#build_fields,)*
+    };
+
     let q = quote! {
         use std::io::Cursor;
         use anyhow::Result;
@@ -16,8 +44,7 @@ pub fn derive_parse(input: TokenStream) -> TokenStream {
                 let mut c = Cursor::new(bytes);
                 let result = reader::read_wire_binary(&mut c)?;
 
-                let mut s: u32 = 0;
-                let mut x: i64 = 0;
+                #init_fields
                 for sw in result {
                     match (sw.field_number(), sw.wire_type()) {
                         (1, reader::WireType::Varint(v)) => {
@@ -29,7 +56,9 @@ pub fn derive_parse(input: TokenStream) -> TokenStream {
                         _ => (),
                     }
                 }
-                Ok(Self { s, x })
+                Ok(Self {
+                    #build_fields
+                })
             }
             pub fn bytes(&self)-> Vec<u8>{
                 Vec::new()
