c159c9ff8105ceddf50c6a246745745606066ef2
blck-snwmn <whitesnowmancreator@gmail.com>
2021-04-13 00:23:14 +0900

    Add zigzag trait

--- src/zigzag.rs
+++ src/zigzag.rs
@@ -1,13 +1,42 @@
+use std::{
+    mem::{size_of, size_of_val},
+    ops::{Shl, Shr},
+};
+
 pub(crate) fn encode32(n: i32) -> u32 {
     ((n << 1) ^ (n >> 31)) as u32
 }
 pub(crate) fn encode64(n: i64) -> u64 {
     ((n << 1) ^ (n >> 63)) as u64
 }
+
+pub(crate) fn encode<T: ZigZag>(n: T) -> T::Output {
+    n.encode()
+}
+
 pub(crate) fn decode(n: u128) -> i128 {
     let r = (n >> 1) as i128;
     let l = (n & 1) as i128;
     (r ^ -l) as i128
+}
+
+pub trait ZigZag {
+    type Output;
+    fn encode(&self) -> Self::Output;
+}
+
+impl ZigZag for i32 {
+    type Output = u32;
+    fn encode(&self) -> Self::Output {
+        ((self << 1) ^ (self >> 31)) as u32
+    }
+}
+
+impl ZigZag for i64 {
+    type Output = u64;
+    fn encode(&self) -> Self::Output {
+        ((self << 1) ^ (self >> 63)) as u64
+    }
 }
 
 #[cfg(test)]
@@ -33,6 +62,21 @@ mod tests {
         assert_eq!(encode64(2147483647), 4294967294);
         assert_eq!(encode64(-9223372036854775808), 18446744073709551615);
         assert_eq!(encode64(9223372036854775807), 18446744073709551614);
+    }
+
+    #[test]
+    fn test_encode() {
+        assert_eq!(encode(0), 0 as u32);
+        assert_eq!(encode(-1), 1 as u32);
+        assert_eq!(encode(1), 2 as u32);
+        assert_eq!(encode(-2), 3 as u32);
+        assert_eq!(encode(2), 4 as u32);
+        assert_eq!(encode(2147483647), 4294967294 as u32);
+        assert_eq!(encode(-2147483648), 4294967295 as u32);
+        assert_eq!(encode(2147483647 as i32), 4294967294);
+        assert_eq!(encode(-2147483648 as i32), 4294967295);
+        assert_eq!(encode(-9223372036854775808 as i64), 18446744073709551615);
+        assert_eq!(encode(9223372036854775807 as i64), 18446744073709551614);
     }
     #[test]
     fn test_decode_zigzag() {
